cfebecb36e0a1d3d834bfdffcbf53921b59b8ac3
Gabriel Ruschel <gabrielrcastanhel@gmail.com>
2025-05-07 11:26:09 -0300

    test: add rover rotation unit tests

--- internal/rover/rover_test.go
+++ internal/rover/rover_test.go
@@ -60,3 +60,95 @@ func TestParseRoverPosition(t *testing.T) {
 		})
 	}
 }
+
+func TestRotateRover(t *testing.T) {
+	tCases := []struct {
+		name              string
+		direction         rune
+		currOrient        rune
+		currOrientIdx     int
+		expectedOrient    rune
+		expectedOrientIdx int
+	}{
+		{
+			name:              "Test rotate LEFT from NORTH",
+			direction:         Left,
+			currOrient:        North,
+			currOrientIdx:     OrientationIdx[North],
+			expectedOrient:    West,
+			expectedOrientIdx: OrientationIdx[West],
+		},
+		{
+			name:              "Test rotate LEFT from WEST",
+			direction:         Left,
+			currOrient:        West,
+			currOrientIdx:     OrientationIdx[West],
+			expectedOrient:    South,
+			expectedOrientIdx: OrientationIdx[South],
+		},
+		{
+			name:              "Test rotate LEFT from SOUTH",
+			direction:         Left,
+			currOrient:        South,
+			currOrientIdx:     OrientationIdx[South],
+			expectedOrient:    East,
+			expectedOrientIdx: OrientationIdx[East],
+		},
+		{
+			name:              "Test rotate LEFT from EAST",
+			direction:         Left,
+			currOrient:        East,
+			currOrientIdx:     OrientationIdx[East],
+			expectedOrient:    North,
+			expectedOrientIdx: OrientationIdx[North],
+		},
+
+		// ---------------------------------------------------------
+
+		{
+			name:              "Test rotate RIGHT from NORTH",
+			direction:         Right,
+			currOrient:        North,
+			currOrientIdx:     OrientationIdx[North],
+			expectedOrient:    East,
+			expectedOrientIdx: OrientationIdx[East],
+		},
+		{
+			name:              "Test rotate RIGHT from EAST",
+			direction:         Right,
+			currOrient:        East,
+			currOrientIdx:     OrientationIdx[East],
+			expectedOrient:    South,
+			expectedOrientIdx: OrientationIdx[South],
+		},
+		{
+			name:              "Test rotate RIGHT from SOUTH",
+			direction:         Right,
+			currOrient:        South,
+			currOrientIdx:     OrientationIdx[South],
+			expectedOrient:    West,
+			expectedOrientIdx: OrientationIdx[West],
+		},
+		{
+			name:              "Test rotate RIGHT from WEST",
+			direction:         Right,
+			currOrient:        West,
+			currOrientIdx:     OrientationIdx[West],
+			expectedOrient:    North,
+			expectedOrientIdx: OrientationIdx[North],
+		},
+	}
+
+	for _, tc := range tCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rov := &Rover{
+				Orientation:    tc.currOrient,
+				orientationIdx: tc.currOrientIdx,
+			}
+
+			rov.rotateRover(tc.direction)
+			assert.Equal(t, tc.expectedOrient, rov.Orientation)
+			assert.Equal(t, tc.expectedOrientIdx, rov.orientationIdx)
+		})
+	}
+}
